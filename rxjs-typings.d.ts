06b56bb8c01075c3bce826cde1bf6458fecda429
Alex0007 <Alex0007lolpvp@gmail.com>
2017-10-02 21:28:33 +0500

    typings for rxjs

--- rxjs-typings.d.ts
+++ rxjs-typings.d.ts
@@ -2,6 +2,6 @@ import { Observable } from 'rxjs';
 import { ResponseStreamBase } from './src/interfaces';
 
 export interface AsyncDriverSource<Req, Res> {
-    filter<S extends AsyncDriverSource<Req, Res>>(predicate: (request: Req) => boolean): S;
+    filter(predicate: (request: Req) => boolean): AsyncDriverSource<Req, Res>;
     select(category?: string): Observable<Observable<Res> & ResponseStreamBase<Req>>;
 }
